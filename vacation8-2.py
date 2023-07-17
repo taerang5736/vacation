fd18f72f5a0928602b16ad6cf6c773e2138f8579
taerang5736 <128353157+taerang5736@users.noreply.github.com>
2023-07-17 17:11:57 +0900

    Add files via upload

---
+++ vacation8-2.py
@@ -0,0 +1,15 @@
+'''money = 2000
+card = 1
+if 4 in [1,2,3]:
+    print("택시를 타고 가라")
+else:
+    print("walking !")''' 
+    
+pocket = ['paper','cellphone']
+card = True
+if 'money' in pocket:
+    pass
+elif card:
+    print("택시를 타고 가라")
+else:
+       print("walking !") 
